bb2196d60078194afbef2ff960accd52afe1b937
Dan Galera <danerick.galera@gmail.com>
2021-03-05 14:36:43 +0800

    Refactored devise registration controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     protected
   
     def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :gender, :mobile)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :gender, :mobile)}
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :gender_id, :mobile)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :gender_id, :mobile)}
     end
 end
