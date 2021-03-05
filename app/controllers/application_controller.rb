c21b0baedf76a8494709b68bf75b70c8eff3c457
Dan Galera <danerick.galera@gmail.com>
2021-03-05 14:40:42 +0800

    Moves devise registration params configs to users/registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-
-    before_action :update_allowed_parameters, if: :devise_controller?
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :gender_id, :mobile)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :gender_id, :mobile)}
-    end
 end
